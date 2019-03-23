be7eb058c4955c2a979b0b588c56d852ac6062a2
Adamkzh <mustkzh@gmail.com>
2019-03-23 16:55:40 -0700

    client axios get func

--- clientn/src/components/Panel-Detail-Dashboard.js
+++ clientn/src/components/Panel-Detail-Dashboard.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import axios from 'axios';
+
+
+
 
 class Dashboard extends Component{
+
+componentDidMount(){
+    var id = this.props.match.params.id;
+    if(id !== "admin"){
+        axios.get('/api/get',{
+            params:{
+                id: id
+            }
+        })
+        .then(function (response) {
+           console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+}
 
     render(){
         return(
             <div>
                 <Header activeTag={"dashboard"}/>
-                {this.props.match.params.id}
             </div>
         );
     }
