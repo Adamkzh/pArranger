711598685a1ee20cbcb2be1f5c98d5e65a6b93f8
Adamkzh <mustkzh@gmail.com>
2019-03-28 19:23:06 -0700

    temp fix the input address bug

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,7 @@ import { Container, Menu, Button } from 'semantic-ui-react'
 
 
 var activeTag = "home";
+var idLink = "/";
 
 class HomepageHeading extends Component {
 
@@ -16,7 +17,12 @@ class HomepageHeading extends Component {
         if(window.localStorage.getItem('step') === undefined){
            window.localStorage.setItem('step',0);
         }
-    
+    }
+
+    componentDidMount = ()=>{
+      if(window.localStorage.getItem('uuid') !== null){
+          idLink = "/dashboard/" + window.localStorage.getItem('uuid');
+      }
     }
     render(){ 
         var fixed = true;
@@ -26,7 +32,7 @@ class HomepageHeading extends Component {
             <Container>
               <Menu.Item as={Link} to='/' active={activeTag === "home"}>HOME</Menu.Item>
               <Menu.Item as={Link} to='/design' active={activeTag === "design"}>DESIGN</Menu.Item>
-              <Menu.Item as={Link} to='/dashboard/admin' active={activeTag === "dashboard"}>DASHBOARD</Menu.Item>
+              <Menu.Item as={Link} to= {idLink} active={activeTag === "dashboard"}>DASHBOARD</Menu.Item>
               <Menu.Item as={Link} to='/search' >SEARCH</Menu.Item>
               <Menu.Item as={Link} to= '/console' active={activeTag === "console"}>CONSOLE</Menu.Item>
               <Menu.Item position='right'>
--- client/src/components/Panel-Detail-Dashboard.js
+++ client/src/components/Panel-Detail-Dashboard.js
@@ -22,35 +22,36 @@ constructor(props){
 
 
 componentDidMount= ()=> {
-    var id = this.props.match.params.id;
-    if(id !== "admin"){  
-      const getUserByIDUrl = '/api/v1/getUser?id=' + id;
-      axios.get(getUserByIDUrl)
-        .then((response) =>{
-          console.log("[Response] from server: ");
-          console.log(response);
-          if (response.data.success) {
-              const data = response.data.result;
-              this.setState({
-                  address: data.address,
-                  uuid: data._id,
-                  username: data.username,
-                  email: data.email,
-                  watts: data.watts,
-                  mountType: data.mountType,
-                  image:data.mapImage,
-              });
-  
-          } else {
-              console.log(response.data.error);
-          }
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
-        .then(function () {
-          // always executed
-        });  
+  // fetch current user data from server
+  var id = this.props.match.params.id;
+  if(id !== "admin"){  
+    const getUserByIDUrl = '/api/v1/getUser?id=' + id;
+    axios.get(getUserByIDUrl)
+      .then((response) =>{
+        console.log("[Response] from server: ");
+        console.log(response);
+        if (response.data.success) {
+            const data = response.data.result;
+            this.setState({
+                address: data.address,
+                uuid: data._id,
+                username: data.username,
+                email: data.email,
+                watts: data.watts,
+                mountType: data.mountType,
+                image:data.mapImage,
+            });
+
+        } else {
+            console.log(response.data.error);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });  
     }
 
 }
