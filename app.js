f207ef9bec08fffc7b3de17acd42010e057d19c4
IdleDust <juanchen917@gmail.com>
2019-05-13 16:46:04 -0700

    changed env for port

--- app.js
+++ app.js
@@ -54,4 +54,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 module.exports = app;
